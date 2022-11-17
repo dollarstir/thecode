0ee5caeee0019fa01260f9e3f0ebbafd7f64fb09
Frederick Ennin <kpin463@gmail.com>
2022-11-17 08:49:49 +0000

    55

--- template/out.php
+++ template/out.php
@@ -1,6 +1,6 @@
 <?php
 
             session_start();
-               session_destroy();
+               unset($_SESSION['vuser']);
             echo '<script>window.location= "home";</script>';
             // code...
