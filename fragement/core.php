ba452d31e45088127e96b9a859aea0b51869edfc
Frederick Ennin <kpin463@gmail.com>
2022-11-25 07:51:14 +0000

    192

--- fragement/core.php
+++ fragement/core.php
@@ -654,7 +654,7 @@ function checkout($name, $email, $contact, $note, $paymenttype, $password)
                 if (isset($_SESSION['strcart'])) {
                   $cart = $_SESSION['strcart'];
                   $token = bin2hex(random_bytes(8)).time();
-                  $dateadded = date('jS F, Y');
+                  $dateadded = date('jS F, Y').' at '.date('h:i:s A');
                   $cco = countall('vorders');
                   $ordno = $cco + 1;
                   foreach ($cart as $c) {
@@ -698,7 +698,7 @@ function checkout($name, $email, $contact, $note, $paymenttype, $password)
                       if (isset($_SESSION['strcart'])) {
                           $cart = $_SESSION['strcart'];
                           $token = bin2hex(random_bytes(8)).time();
-                          $dateadded = date('jS F, Y');
+                          $dateadded = date('jS F, Y').' at '.date('h:i:s A');
                           $cco = countall('vorders');
                           $ordno = $cco + 1;
                           foreach ($cart as $c) {
